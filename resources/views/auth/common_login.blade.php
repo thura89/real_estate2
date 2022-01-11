60881ca5a59b15e365c6f8ed38176d2187e75eec
thura89 <mozet89@gmail.com>
2022-01-11 09:27:40 +0630

    google adsence test

--- resources/views/auth/common_login.blade.php
+++ resources/views/auth/common_login.blade.php
@@ -13,6 +13,8 @@
 	<link rel="stylesheet" href="{{asset('/backend/css/style.css')}}">
     <link href="{{ asset('backend/css/bootstrap.min.css')}}" rel="stylesheet">
 
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5902016702314933"
+     crossorigin="anonymous"></script>
 
 </head>
 
