33af884d3e0eb7e53a05448f093a957a0efe735b
thura89 <mozet89@gmail.com>
2022-08-20 19:29:06 +0630

    add csrf token

--- resources/views/auth/common_login.blade.php
+++ resources/views/auth/common_login.blade.php
@@ -2,10 +2,10 @@
 <html lang="en">
 
 <head>
-    <title>Common Login</title>
+    <title>Tinemyay Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
