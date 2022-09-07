541c288cf77544edb9dfaf0870fea90912c06628
thura89 <mozet89@gmail.com>
2022-09-07 17:23:48 +0630

    change header logo name

--- resources/views/backend/agent/layouts/header.blade.php
+++ resources/views/backend/agent/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <div class="app-header header-shadow">
     <div class="app-header__logo">
         {{-- <div class="logo-src"></div> --}}
-        <div class="logo-src"><img src="{{ asset('backend/images/timemyay_logo.png') }}" alt=""><span class="logo-text">TimeMyay</span></div>
+        <div class="logo-src"><img src="{{ asset('backend/images/timemyay_logo.png') }}" alt=""><span class="logo-text">TineMyay</span></div>
         {{-- <h1>Y</h1><h2>O<h2><h3>Y</h3><h4>O</h4><span class="badge">Real Estate</span> --}}
         <div class="header__pane ml-auto">
             <div>
