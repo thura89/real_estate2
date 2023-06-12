34d7804762d17733d8c7c3e5156721b23feac1f7
Thura <thura.pyaetoe@yathar.com>
2023-06-12 15:20:13 +0630

    add areasize to wishlist

--- app/Http/Resources/API/PropertyWishlist.php
+++ app/Http/Resources/API/PropertyWishlist.php
@@ -34,6 +34,22 @@ class PropertyWishlist extends JsonResource
 
         /* wishlist splice */
         $wishlist = $this->wishlist()->first('id');
+
+        /* AreaSize */
+        if ($this->areasize) {
+            if ($this->areasize->area_option != NULL) {
+                if ($this->areasize->area_option == 1) {
+                    $area_size =  $this->areasize->width . ' x ' . $this->areasize->length;
+                }
+                if ($this->areasize->area_option == 2) {
+                    if ($this->areasize->area_unit > 2) {
+                        $area_size =  $this->areasize->area_size . ' ' . '-';
+                    } else {
+                        $area_size =  $this->areasize->area_size . ' ' . config('const.area_short')[$this->areasize->area_unit];
+                    }
+                }
+            }
+        }
         
         return [
             'wishlist_id' => $wishlist->id ?? null,
@@ -46,7 +62,7 @@ class PropertyWishlist extends JsonResource
             'category' => config('const.property_category')[$this->category],
             'bed_room' => $this->partation->bed_room ?? null,
             'bath_room' => $this->partation->bath_room ?? null,
-            // 'carpark' => $this->partation->carpark ?? null,
+            'area_size' => $area_size,
             'created_at' => Carbon::parse($this->created_at)->format('Y-m-d H:m:s'),
         ];
     }
--- app/Http/Resources/PropertiesDataByRelated.php
+++ app/Http/Resources/PropertiesDataByRelated.php
@@ -35,6 +35,22 @@ class PropertiesDataByRelated extends JsonResource
         /* Region */
         $region = $this->address ? $this->address->region()->first('name') : null;
 
+        /* AreaSize */
+        if ($this->areasize) {
+            if ($this->areasize->area_option != NULL) {
+                if ($this->areasize->area_option == 1) {
+                    $area_size =  $this->areasize->width . ' x ' . $this->areasize->length;
+                }
+                if ($this->areasize->area_option == 2) {
+                    if ($this->areasize->area_unit > 2) {
+                        $area_size =  $this->areasize->area_size . ' ' . '-';
+                    } else {
+                        $area_size =  $this->areasize->area_size . ' ' . config('const.area_short')[$this->areasize->area_unit];
+                    }
+                }
+            }
+        }
+
         $data['id'] = $this->id;
         $data['user'] = $this->user_id;
         $data['title'] = $this->title;
@@ -46,6 +62,7 @@ class PropertiesDataByRelated extends JsonResource
         $data['category'] = config('const.property_category')[$this->category];
         $data['bed_room'] = $this->partation->bed_room ?? null;
         $data['bath_room'] = $this->partation->bath_room ?? null;
+        $data['area_size'] = $area_size ?? null;
         if (Auth::guard('api')->check()) {
             $favorite = WishList::where('user_id', Auth::guard('api')->user()->id)->where('property_id', $this->id)->first();
             if ($favorite) {
